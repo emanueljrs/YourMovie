6ad05d94ced19bf184b308a58d6fdfb9109a4aeb
Emanuel Rodrigues <emanuelrootsneite@gmail.com>
2021-06-01 05:13:36 -0300

    Created project and created layout activity_movie.xml

---
+++ app/src/main/java/com/emanuel/yourmovie/ui/movie/MovieActivity.kt
@@ -0,0 +1,12 @@
+package com.emanuel.yourmovie.ui.movie
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.emanuel.yourmovie.R
+
+class MovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+    }
+}
